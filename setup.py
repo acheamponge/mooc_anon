6e8ab4ccbaf2b2d08be8821c35304c7111459eea
acheamponge <acheamponge@berea.edu>
2019-07-07 20:03:05 -0400

    redoing the mooc module on pip

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='mooc_anon',
+     version='0.1',
+     scripts=['mooc_anon.py'] ,
+     author="Emmanuel Acheampong",
+     author_email="achampion.emma@gmail.com",
+     description="MOOC Anonymous",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/javatechy/dokr",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
